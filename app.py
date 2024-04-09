0004590231ff0633d0b25a58a9c018dce990980c
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-10 00:46:22 +0100

    testing

--- app.py
+++ app.py
@@ -51,7 +51,8 @@ def room(id):
    cur = mysql.cursor() #create a connection to the SQL instance
    cur.execute("SELECT * FROM Room WHERE RoomNo = %s", [id]) # execute an SQL statment
    data = cur.fetchall()
-   return render_template('room.html', details=data)
+   test = json.dumps(data)
+   return render_template('room.html', details=test)
 
 @app.errorhandler(404)
 def not_found_error(error):
