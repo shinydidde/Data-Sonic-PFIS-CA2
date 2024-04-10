004b9e232865e2ad54c4c60a2decccacf6885c6a
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-10 14:19:10 +0100

    adding login

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
+from google.auth.transport import requests
+from google.cloud import datastore
+import google.oauth2.id_token
 import mysql.connector
 from flask_cors import CORS
 import json
@@ -39,12 +40,41 @@ def get_rooms():
     return jsonify(rooms_data)
 
 # Serve index.html file
+firebase_request_adapter = requests.Request()
 @app.route('/')
 def index():
     cur = mysql.cursor() #create a connection to the SQL instance
     cur.execute('''SELECT * FROM room''') # execute an SQL statment
     data = cur.fetchall()
-    return render_template('index.html', room=data)
+    # Verify Firebase auth.
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    times = None
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter
+            )
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+
+        # Record and fetch the recent times a logged-in user has accessed
+        # the site. This is currently shared amongst all users, but will be
+        # individualized in a following step.
+        store_time(datetime.datetime.now(tz=datetime.timezone.utc))
+        times = fetch_times(10)
+
+    return render_template(
+        "index.html", user_data=claims, error_message=error_message, times=times, room=data
+    )
 
 @app.route('/room/<id>')
 def room(id):
