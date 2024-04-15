a66b03ce7a2373bde8f7c150507dfe30e15b1824
Gnanasekar Mani <77843209+gnanas1295@users.noreply.github.com>
2024-04-15 10:03:20 +0100

    Updated the port to 8080

--- app.py
+++ app.py
@@ -169,6 +169,6 @@ def not_found_error(error):
 if __name__ == "__main__":
 
 #   app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
-  # app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
-  app.run(host='0.0.0.0',port='17234', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+  app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+  # app.run(host='0.0.0.0',port='17234', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
 
