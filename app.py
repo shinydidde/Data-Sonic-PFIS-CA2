9a3e71a6d646fc254a139ff8aac0a37db4b3ea25
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-17 18:30:50 +0100

    Bookings page

--- app.py
+++ app.py
@@ -179,6 +179,20 @@ def availability():
         print(startDate,endDate)
     return redirect(url_for('index'))
 
+
+@app.route('/book', methods=['POST'])
+def book():
+    if request.method == 'POST':
+        # Get form data
+        name = request.form['name']
+        email = request.form['email']
+        check_in = request.form['check_in']
+        check_out = request.form['check_out']
+        room_type = request.form['room_type']
+
+        # Redirect to a thank you page or confirmation page
+        return render_template('booking-confirmation.html', name=name)
+
 # Route for the Booking for the user: /user/booking
 # Functionalities will be to return the room details with type and availablity for the seletected range.
 
