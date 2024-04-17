7443c0f51bfff5e70cd7e7eb8fbc464aa2c8f3be
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-17 15:32:39 +0100

    Datepicker

--- static/user.js
+++ static/user.js
@@ -1,11 +1,28 @@
-$(document).ready(function() {
-    $('#startdate').on('change', function() {
-        var startDate = new Date($(this).val());
-        var minEndDate = new Date(startDate.getTime() + (24 * 60 * 60 * 1000)); // Add 1 day to start date
-        $('#enddate').attr('min', minEndDate.toISOString().split('T')[0]); // Set min date for end date
+// $(document).ready(function() {
+//     $('#startdate').on('change', function() {
+//         var startDate = new Date($(this).val());
+//         var minEndDate = new Date(startDate.getTime() + (24 * 60 * 60 * 1000)); // Add 1 day to start date
+//         $('#enddate').attr('min', minEndDate.toISOString().split('T')[0]); // Set min date for end date
+//     });
+
+//     // Prevent user from selecting previous dates for start date
+//     var today = new Date().toISOString().split('T')[0];
+//     $('#startdate').attr('min', today);
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    flatpickr('#startdate', {
+        minDate: 'today',
+        onChange: function(selectedDates, dateStr) {
+            var minEndDate = new Date(selectedDates[0].getTime() + (24 * 60 * 60 * 1000));
+            flatpickr('#enddate', {
+                minDate: minEndDate,
+                defaultDate: minEndDate,
+            });
+        }
     });
 
-    // Prevent user from selecting previous dates for start date
-    var today = new Date().toISOString().split('T')[0];
-    $('#startdate').attr('min', today);
+    flatpickr('#enddate', {
+        minDate: 'today',
+    });
 });
