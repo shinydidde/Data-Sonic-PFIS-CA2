79524bca829c6f8b2470a77cad6189f8dc6e610e
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-18 17:55:39 +0100

    Booking Room

--- static/booking.js
+++ static/booking.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
         // Get the start and end dates from the form
         var startDate = sessionStorage.getItem('startDate');
         var endDate = sessionStorage.getItem('endDate');
-        window.location.href = $(this).attr("href") + encodeURIComponent(startDate) + '&endDate=' + encodeURIComponent(endDate);;;
+        window.location.href = $(this).attr("href") + '?startDate=' + encodeURIComponent(startDate) + '&endDate=' + encodeURIComponent(endDate);;;
     });
 });
