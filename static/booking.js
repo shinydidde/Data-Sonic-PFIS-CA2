fa8ba47097b3d2c2ac78469accaddf51537e181c
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-18 20:57:51 +0100

    Booking Room

--- static/booking.js
+++ static/booking.js
@@ -12,45 +12,4 @@ $(document).ready(function () {
     $("#check_in").val(startDate);
     $("#check_out").val(endDate);
 
-    var roomSelect = document.getElementById('room_type');
-    var availability = $(this).data('availability');
-    var addRoomButton = document.getElementById('addRoom');
-    var deleteRoomButton = document.getElementById('deleteRoom');
-
-    // Populate the select dropdown with room types
-    function populateRoomOptions() {
-        roomSelect.innerHTML = '';
-        for (var room in availability) {
-            var option = document.createElement('option');
-            option.value = room;
-            option.textContent = room + ' - ';
-            if (availability[room] > 0) {
-                option.textContent += 'Available';
-            } else if (availability[room] == 0) {
-                option.textContent += 'Fully Booked';
-            } else {
-                option.textContent += 'Not Available';
-            }
-            roomSelect.appendChild(option);
-        }
-    }
-
-    // Function to add a new room
-    addRoomButton.addEventListener('click', function() {
-        var newRoom = prompt('Enter the new room type:');
-        if (newRoom && !availability[newRoom]) {
-            availability[newRoom] = 1;
-            populateRoomOptions();
-        }
-    });
-
-    // Function to delete a room
-    deleteRoomButton.addEventListener('click', function() {
-        var selectedRoom = roomSelect.value;
-        if (selectedRoom && confirm('Are you sure you want to delete ' + selectedRoom + '?')) {
-            delete availability[selectedRoom];
-            populateRoomOptions();
-        }
-    });
-
 });
