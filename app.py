10feeb3e62c5b5af22f53f9e923bf0bc4fc9c330
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-21 17:15:17 +0100

    Manage Booking

--- app.py
+++ app.py
@@ -350,7 +350,7 @@ def confirmation():
     return render_template('booking-confirmation.html',name = "test", email = "Testing",check_in= "2024-01-01",check_out= "2024-01-01",room_type= "Suite Room")
 
 
-@app.route('/manage-booking/<token>', methods=['GET','POST'])
+@app.route('/manage-booking/<token>', methods=['POST'])
 def booking_confirmation(token):
     username = request.form.get('username')
     # Example validation: Check if the username is not empty
@@ -361,8 +361,8 @@ def booking_confirmation(token):
             bookedDetails = bookingView(token)
             print("Booked Details", bookedDetails)
             return render_template('manage-booking.html', token=token)
-    else:
-        return render_template('index.html')
+        else:
+            return render_template('index.html')
     # Render the booking confirmation template with the token
 
 
