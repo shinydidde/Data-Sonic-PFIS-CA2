ae5f2ecdee71e4954f35193a977eb282f575e967
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-04-22 00:24:14 +0100

    Manage Booking

--- static/manage-booking.js
+++ static/manage-booking.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
+
+    var userName = prompt("Please enter your name mentioned in your booking", "");
+    var currentUrl = window.location.href;
+    var urlObject = new URL(currentUrl);
+    var pathname = urlObject.pathname;
+    let token
+    if (pathname && pathname !== '/') {
+        token = pathname.split('/').pop();
+    }
+
     // Function to prompt alert for user's name
     function promptUserName() {
-        var userName = prompt("Please enter your name mentioned in your booking", "");
-        var currentUrl = window.location.href;
-        var urlObject = new URL(currentUrl);
-        var pathname = urlObject.pathname;
-        if (pathname && pathname !== '/') {
-            var token = pathname.split('/').pop();
-        }
+
         if (userName != null && userName != "") {
             $.ajax({
                 type: "POST",
@@ -15,24 +19,24 @@ $(document).ready(function () {
                 data: { username: userName, token: token },
                 success: function (response) {
                     if (response.valid) {
-                            $(".change-title").text('Edit Booking');
-                            var name = response.name;
-                            var price = response.price;
-                            var email = response.email;
-                            var room_type = response.room_type;
-                            var check_in = response.check_in;
-                            var check_out = response.check_out;
-                            var note = response.note;
+                        $(".change-title").text('Edit Booking');
+                        var name = response.name;
+                        var price = response.price;
+                        var email = response.email;
+                        var room_type = response.room_type;
+                        var check_in = response.check_in;
+                        var check_out = response.check_out;
+                        var note = response.note;
 
-                            $("#name").val(name);
-                            $("#email").val(email);
-                            $("#price").val(price);
-                            $("#room_type").val(room_type);
-                            $("#check_in").val(check_in);
-                            $("#check_out").val(check_out);
-                            $("#note").val(note);
-                            $("#type").val('edit');
-                            $("#manage-booking-form").show();
+                        $("#name").val(name);
+                        $("#email").val(email);
+                        $("#price").val(price);
+                        $("#room_type").val(room_type);
+                        $("#check_in").val(check_in);
+                        $("#check_out").val(check_out);
+                        $("#note").val(note);
+                        $("#type").val('edit');
+                        $("#manage-booking-form").show();
                     } else {
                         alert("Invalid username. Please try again.");
                     }
@@ -40,9 +44,6 @@ $(document).ready(function () {
             });
         }
     }
-
-    // Execute the promptUserName function when the page loads
-    promptUserName();
 
     $(".update-booking").click(function () {
         $("#token").val(token);
@@ -55,6 +56,9 @@ $(document).ready(function () {
         $("#type").val('delete');
         $("#manage-booking-form").submit();
     });
+
+    // Execute the promptUserName function when the page loads
+    promptUserName();
 
 });
 
